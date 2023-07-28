5c8f7000adec4b6a9db53333b99864e224aee95f
xoldyckk <xoldyckk@gmail.com>
2023-07-28 20:30:00 +0530

    first

---
+++ exercises/enums/enums1.rs
@@ -0,0 +1,18 @@
+// enums1.rs
+//
+// No hints this time! ;)
+
+#[derive(Debug)]
+enum Message {
+    ChangeColor,
+    Echo,
+    Move,
+    Quit,
+}
+
+fn main() {
+    println!("{:?}", Message::Quit);
+    println!("{:?}", Message::Echo);
+    println!("{:?}", Message::Move);
+    println!("{:?}", Message::ChangeColor);
+}
---
+++ exercises/macros/macros4.rs
@@ -0,0 +1,32 @@
+// macros4.rs
+//
+// Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
+// hint.
+
+#[rustfmt::skip]
+macro_rules! my_macro {
+    () => {
+        println!("Check out my macro!");
+    };
+    ($val:expr) => {
+        println!("Look at this other macro: {}", $val);
+    }
+}
+
+fn main() {
+    my_macro!();
+    my_macro!(7777);
+}
+
+// #[macro_export]
+// macro_rules! vec {
+//     ( $( $x:expr ),* ) => {
+//         {
+//             let mut temp_vec = Vec::new();
+//             $(
+//                 temp_vec.push($x);
+//             )*
+//             temp_vec
+//         }
+//     };
+// }
